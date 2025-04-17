0a5c57a6d8668a6e1085460359295cbaeb85b8f9
captivator <84224501+qaptivator@users.noreply.github.com>
2025-04-17 21:33:52 +0300

    more changes

--- src/main/java/org/qaptivator/everyexport/Everyexport.java
+++ src/main/java/org/qaptivator/everyexport/Everyexport.java
@@ -3,6 +3,7 @@ package org.qaptivator.everyexport;
 import com.mojang.brigadier.arguments.StringArgumentType;
 import net.fabricmc.api.ModInitializer;
 import net.fabricmc.fabric.api.command.v2.CommandRegistrationCallback;
+import net.minecraft.command.CommandSource;
 import net.minecraft.registry.Registries;
 import net.minecraft.recipe.RecipeManager;
 import net.minecraft.recipe.Recipe;
@@ -13,8 +14,8 @@ import com.mojang.brigadier.suggestion.SuggestionProvider;
 import net.minecraft.text.ClickEvent;
 import net.minecraft.text.Text;
 import net.minecraft.util.Identifier;
-import net.minecraft.command.CommandRegistryAccess;
-import net.minecraft.server.command.CommandManager.RegistrationEnvironment;
+//import net.minecraft.command.CommandRegistryAccess;
+//import net.minecraft.server.command.CommandManager.RegistrationEnvironment;
 
 import java.io.File;
 import java.io.FileWriter;
@@ -26,20 +27,21 @@ import java.io.IOException;
 
 import static net.minecraft.server.command.CommandManager.literal;
 
-private static final SuggestionProvider<ServerCommandSource> EXPORT_TYPE_SUGGESTIONS = (context, builder) -> {
-    return CommandSource.suggestMatching(new String[]{"all", "items", "recipes"}, builder);
-};
-
 public class Everyexport implements ModInitializer {
+
+    private static final SuggestionProvider<ServerCommandSource> EXPORT_TYPE_SUGGESTIONS = (context, builder) -> {
+        return CommandSource.suggestMatching(new String[]{"all", "items", "recipes"}, builder);
+    };
 
     @Override
     public void onInitialize() {
         CommandRegistrationCallback.EVENT.register((dispatcher, registryAccess, environment) -> {
             dispatcher.register(literal("everyexport")
+                    //.requires(source -> source.hasPermissionLevel(2))
                     .then(CommandManager.argument("type", StringArgumentType.word())
                             .suggests(EXPORT_TYPE_SUGGESTIONS)
                             .then(CommandManager.argument("folder", StringArgumentType.string())
-                                    .executes(context -> {
+                                .executes(context -> {
                                         String type = StringArgumentType.getString(context, "type");
                                         String folder = StringArgumentType.getString(context, "folder");
                                         ServerCommandSource source = context.getSource();
@@ -52,20 +54,39 @@ public class Everyexport implements ModInitializer {
                                             if (type.equals("all") || type.equals("recipes")) {
                                                 exportRecipes(folder, source);
                                             }
-                                            ClickEvent.Action.OPEN_FILE
 
                                             Text clickable = Text.literal("[Open folder]")
                                                     .styled(style -> style.withColor(0x00AAFF)
-                                                            .withClickEvent(new ClickEvent(ClickEvent.Action.RUN_COMMAND, "/open_folder " + folder))
+                                                            .withClickEvent(new ClickEvent(ClickEvent.Action.RUN_COMMAND, "/everyexport_openfolder " + folder))
                                                             .withUnderline(true));
 
                                             player.sendMessage(Text.of("[EveryExport] Exported " + type + " to folder: " + folder), false);
+                                            player.sendMessage(clickable, false);
                                         } catch (Exception e) {
                                             player.sendMessage(Text.of("[EveryExport] Error: " + e.getMessage()), false);
                                         }
 
                                         return 1;
                                     }))));
+
+            dispatcher.register(literal("everyexport_openfolder")
+                    //.requires(source -> source.hasPermissionLevel(2))
+                    .then(CommandManager.argument("folder", StringArgumentType.string())
+                            .executes(context -> {
+                                String folder = StringArgumentType.getString(context, "folder");
+                                File dir = Paths.get("config", folder).toFile();
+                                try {
+                                    if (dir.exists()) {
+                                        Desktop.getDesktop().open(dir);
+                                        context.getSource().sendFeedback(() -> Text.of("📂 Opened folder: " + dir.getAbsolutePath()), false);
+                                    } else {
+                                        context.getSource().getPlayer().sendMessage(Text.of("Folder does not exist: " + dir.getAbsolutePath()));
+                                    }
+                                } catch (IOException e) {
+                                    context.getSource().getPlayer().sendMessage(Text.of("Failed to open folder: " + e.getMessage()));
+                                }
+                                return 1;
+                            })));
         });
     }
 
@@ -93,12 +114,13 @@ public class Everyexport implements ModInitializer {
         File recipesFile = new File(dir, "recipes.json");
         FileWriter writer = new FileWriter(recipesFile);
 
+        var registryManager = source.getServer().getRegistryManager();
         RecipeManager recipeManager = source.getServer().getRecipeManager();
         Map<String, Object> recipes = new HashMap<>();
 
         for (Recipe<?> recipe : recipeManager.values()) {
             Identifier id = recipe.getId();
-            recipes.put(id.toString(), recipe.getOutput(null).toString());
+            recipes.put(id.toString(), recipe.getOutput(registryManager).toString());
         }
 
         writer.write(toJson(recipes));
